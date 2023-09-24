617f040426073991b4c8dd902bb1097c5d735fb4
古山裕太 <koyamayuuta@furuyamayuutanoMacBook-Air.local>
2023-09-24 21:38:32 +0900

    third commit

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,8 +1,8 @@
 let num = Math.floor(Math.random() * 20);
 
 //numを3で割ったらあまりが0になる＝3の倍数
-if (num % 3 == 0) {
-  console.log('3の倍数です');
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log('3と5の倍数です');
 }
 
 //numを5で割ったらあまりが0になる＝5の倍数
@@ -11,8 +11,8 @@ else if (num % 5 == 0) {
 }
 
 //numを3または5で割ったらあまりが0になる＝3と5の倍数
-else if (num % 3 == 0 && num % 5 == 0) {
-  console.log('3と5の倍数です');
+else if (num % 3 == 0 ) {
+  console.log('3の倍数です');
 }
 
 //それ以外の場合の変数の値を出力する
